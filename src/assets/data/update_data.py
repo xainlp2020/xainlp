7e39776b32f593b59d292946ab80fea13ddeb992
Ban <bkawas@us.ibm.com>
2020-07-08 15:45:52 -0700

    WIP python script to generate citation network

--- src/assets/data/update_data.py
+++ src/assets/data/update_data.py
@@ -1,6 +1,7 @@
 from scholarly import scholarly
 import json
-
+import time
+import numpy as np
 
 with open('xaipapers.json') as f:
     xai_papers = json.load(f)
@@ -8,6 +9,10 @@ with open('xaipapers.json') as f:
 for taxon in xai_papers:
     for paper in xai_papers[taxon]:
         print(paper['title'])
+        wait = np.random.randint(20,60)
+        print('waiting for {}'.format(wait))
+        time.sleep(wait)
         search_query = scholarly.search_pubs(paper['title'])
-        print(next(search_query))
+        pub = next(search_query)
+        print([citation.bib['title'] for citation in pub.citedby()])
         break
