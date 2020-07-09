d795035bb02674afe0032b19a29ca869a53a05df
kunqian-usa <kunqian.usa@gmail.com>
2020-07-09 10:43:15 -0700

    minor changes to paper collections

--- src/app/home/home.component.ts
+++ src/app/home/home.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import xaipapers from "../../assets/data/xaipapers.json";
 
 @Component({
   selector: 'app-home',
@@ -7,9 +8,38 @@ import { Component, OnInit } from '@angular/core';
 })
 export class HomeComponent implements OnInit {
 
+  total_num_papers = 0
+  total_search = 109
+
+  all_papers = []
+  
+  load_all_papers()
+  {
+    for(var i = 0; i < xaipapers['local-post-hoc'].length; i++)
+    {
+      this.all_papers.push(xaipapers["local-post-hoc"][i])
+    }
+    for(var i = 0; i < xaipapers['local-self'].length; i++)
+    {
+      this.all_papers.push(xaipapers["local-self"][i])
+    }
+    for(var i = 0; i < xaipapers['global-post-hoc'].length; i++)
+    {
+      this.all_papers.push(xaipapers["global-post-hoc"][i])
+    }
+    for(var i = 0; i < xaipapers['global-self'].length; i++)
+    {
+      this.all_papers.push(xaipapers["global-self"][i])
+    }
+    // var tmp = JSON.stringify(this.all_papers).toLowerCase()
+    // this.all_papers = JSON.parse(tmp)
+    this.total_num_papers = this.all_papers.length
+  }
+  
   constructor() { }
 
   ngOnInit() {
+    this.load_all_papers();
   }
 
 }
