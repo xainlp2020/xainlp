7373529b53f6b6aece7404eb0e7fccd0b8e07692
Yannis Katsis <Yannis.Katsis@ibm.com>
2020-07-08 21:50:05 -0700

    Add tabs to definitions page

--- src/app/definitions/definitions.component.ts
+++ src/app/definitions/definitions.component.ts
@@ -1,9 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
+import { MatTabsModule } from '@angular/material/tabs';
+import { NgModule } from '@angular/core';
 
 @Component({
   selector: 'app-definitions',
   templateUrl: './definitions.component.html',
-  styleUrls: ['./definitions.component.css']
+  styleUrls: ['./definitions.component.css'],
+  encapsulation: ViewEncapsulation.None
 })
 export class DefinitionsComponent implements OnInit {
 
